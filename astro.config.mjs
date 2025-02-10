57643ac09e389730327fb0a7215d7dccb12d2cda
kennycallado <kennycallado@hotmail.com>
2025-02-10 12:43:18 +0100

    TEST

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, sharpImageService } from "astro/config";
 import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
   server: {
     port: 3000,
+  },
+
+  image: {
+    service: sharpImageService(),
+    remotePatterns: [{ protocol: "https" }],
   },
 
   integrations: [lit()],
